05bb17c9697706c80a096dcc26022d7213d02bbe
peterlind <peter@lind.be>
2012-09-12 20:20:49 +0200

    spec initial behaviour

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,2 +1,9 @@
 class QueriesController < ApplicationController
+  respond_to :json
+
+  def show
+    input = params[:id]
+    query = Query.where(input: input).first
+    respond_with query
+  end
 end
--- spec/controllers/queries_controller_spec.rb
+++ spec/controllers/queries_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe QueriesController do
-
+  context '#new' do
+    context 'cached result' do
+      it 'returns json if requested' do
+        Query.stub(:where).and_return([Query.new( result:'123456-7890')])
+        get :show, id: 'test', format: :json
+        response.should be_successful
+        output = JSON.parse(response.body)
+        output['result'].should == '123456-7890'
+     end
+    end
+  end
 end
