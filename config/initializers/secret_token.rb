e18020156fb1288fddecd7b563f186e9e37a4316
peterlind <peter@lind.be>
2012-09-12 18:05:01 +0200

    rails new....

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abscraper::Application.config.secret_token = '0426ccbaba68d18a2f35403f54fe3bf2291a2dc179319f141945774b0fcdd68661e498dff8d203204efc6f07c8a522d99d845c0a99e4025180a004c66efe3994'
